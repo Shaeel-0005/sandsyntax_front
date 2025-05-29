9e3f4a38409e597768c637f000f9e271f7a808bf
Shaeel Shahid <135009664+Shaeel-0005@users.noreply.github.com>
2025-05-29 14:14:54 +0500

    Adding all Initial components

---
+++ src/components/business.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function business() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default business
---
+++ src/components/stats.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function stats() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default stats
